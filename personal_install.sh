1b4ce8df50c3db8753c1d19a421f1aa1f174efd3
Francis Colas <francis.colas@inria.fr>
2014-11-28 10:40:29 +0100

    Owncloud and KeepassX

---
+++ personal_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# install owncloud
+echo "Install owncloud from http://owncloud.com"
+echo "Copnfigure owncloud client"
+read -p "Press [ENTER] once it's done..."
+
+# install keepassX
+echo "Install KeePassX 2.0 from www.keepassx.org"
+echo "Open database in owncloud"
+read -p "Press [ENTER] once it's done..."
