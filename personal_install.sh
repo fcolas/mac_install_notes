10f6611cdbbf76c00d6d91f3f0c2bc47c0625af0
Francis Colas <francis.colas@inria.fr>
2014-11-28 16:01:10 +0100

    AdBlockPlus

--- personal_install.sh
+++ personal_install.sh
@@ -10,3 +10,7 @@ read -p "Press [ENTER] once it's done..."
 echo "Install KeePassX 2.0 from www.keepassx.org"
 echo "Open database in owncloud"
 read -p "Press [ENTER] once it's done..."
+
+# install AdBlockPlus
+echo "Install AdBlockPlus for Safari (Safari->Safari Extensions)"
+read -p "Press [ENTER] once it's done..."
