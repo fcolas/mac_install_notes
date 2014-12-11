346389a09cf516e5e2a51a247e7857a1726ed1d3
Francis Colas <francis.colas@inria.fr>
2014-12-11 17:14:42 +0100

    update ros_install

--- ros_install.sh
+++ ros_install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 function print_and_run {
-	echo $1
-	#$1
+	echo "$ "$1
+	$1
 }
+Off="\033[0m"
+Info="\033[1;34m"
+Action="\033[0;31m"
 
 # Some dependencies are in generic
-echo "You need to have installed Xcode, brew, pip and Xquartz (cf generic_install) and use python from the system and NOT from brew."
+echo -e $Action"You need to have installed Xcode, brew, pip and Xquartz (cf generic_install) and use python from the system and NOT from brew."$Off
 read -p "Press [ENTER] once it's done..."
 
 # install ROS (wiki.ros.org/indigo/Installation/OSX/Homebrew/Source)
-echo "Installing ros"
 # setting up tools
+echo -e $Info"Installing tools"$Off
 print_and_run 'brew install cmake'
 print_and_run 'brew tap ros/deps'
 print_and_run 'brew tap osrf/simulation'
@@ -21,6 +24,7 @@ print_and_run 'sudo pip install -U wstool rosdep rosinstall rosinstall_generator
 print_and_run 'sudo rosdep init'
 print_and_run 'rosdep update'
 # create workspace
+echo -e $Info"Getting code"$Off
 print_and_run 'mkdir ~/ros_catkin_ws'
 print_and_run 'cd ~/ros_catkin_ws'
 # create list of packages
@@ -30,17 +34,19 @@ print_and_run 'rosinstall_generator rviz --rosdistro hydro --wet-only --tar >> i
 # getting source
 print_and_run 'wstool init -j8 src indigo-desktop-wet.rosinstall'
 # installing dependency
+echo -e $Info"Installing dependencies"$Off
 print_and_run 'rosdep install -r --from-paths src --ignore-src --rosdistro indigo -y'
 # prepare compilation
-echo "Remove tinyxml in src/image_common/image_transport/CMakeLists.txt"
+echo -e $Action"Remove tinyxml in src/image_common/image_transport/CMakeLists.txt"$Off
 read -p "Press [ENTER] once it's done..."
-echo "You may need to check https://github.com/ros-visualization/rviz/pull/825 for rviz to compile."
+echo -e $Action"You may need to check https://github.com/ros-visualization/rviz/pull/825 for rviz to compile."$Off
 read -p "Press [ENTER] once it's done..."
 # actual compilation
+echo -e $Info"Compilation"$Off
 print_and_run './src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release'
 # environment settings
 print_and_run 'source ~/.ros_catkin_ws/install_isolated/setup.bash'
-echo "Permanent ROS environment settings"
+echo -e $Info"Permanent ROS environment settings"$Off
 print_and_run 'echo "# ROS setup" >> ~/.profile'
 print_and_run 'echo "source ~/ros_catkin_ws/install_isolated/setup.bash" >> ~/.profile'
 print_and_run 'echo "export LC_CTYPE=en_US.UTF-8" >> ~/.profile'
