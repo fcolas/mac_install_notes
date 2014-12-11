8d7e256e13da3104193a4e64814129f049ca006f
Francis Colas <francis.colas@inria.fr>
2014-12-11 17:09:34 +0100

    update generic

--- generic_install.sh
+++ generic_install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 function print_and_run {
-	echo $1
-	#$1
+	echo "$ "$1
+	$1
 }
+Off="\033[0m"
+Info="\033[1;34m"
+Action="\033[0;31m"
 
 # first, you need to install Xcode
-echo "You need to install Xcode through the app store before anything else."
+echo -e $Action"You need to install Xcode through the app store before anything else."$Off
 read -p "Press [ENTER] once it's done..."
 
 # you need to run Xcode once to accept the license
-echo "Launch Xcode in order to sign the license agreement."
+echo -e $Action"Launch Xcode in order to sign the license agreement."$Off
 read -p "Press [ENTER] once it's done..."
 
 # installing brew (package manager)
-echo "Installing the Homebrew package manager (brew):"
+echo -e $Info"Installing the Homebrew package manager (brew)"$Off
 print_and_run 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
 # run brew doctor once as requested by the installation
 print_and_run 'brew doctor'
@@ -23,61 +26,61 @@ print_and_run 'echo "# brew shell completion" >> ~/.profile'
 print_and_run 'echo "source `brew --repository`/Library/Contributions/brew_bash_completion.sh" >> ~/.profile'
 
 # installing cask
-echo "Installing cask"
+echo -e $Info"Installing cask"$Off
 print_and_run 'brew install caskroom/cask/brew-cask'
 
 # install vim and macvim
-echo "Install vim and macvim"
+echo -e $Info"Installing vim and macvim"$Off
 print_and_run 'brew install vim macvim'
 # set vim as default editor
 print_and_run 'echo "# default command line editor" >> ~/.profile'
 print_and_run 'echo "export EDITOR=/usr/local/bin/vim" >> ~/.profile'
 
 # install git (with gitk and stuff)
-echo "Installing newer git"
+echo -e $Info"Installing newer git"$Off
 print_and_run 'brew install git'
 
 # completing bash completion
-echo "Completing bash completion"
+echo -e $Info"Completing bash completion"$Off
 print_and_run 'echo "# bash completion" >> ~/.profile'
 print_and_run 'echo "if [ -f $(brew --prefix)/etc/bash_completion ]; then" >> ~/.profile'
 print_and_run 'echo "  . $(brew --prefix)/etc/bash_completion" >> ~/.profile'
 print_and_run 'echo "fi" >> ~/.profile'
 
 # install pip
-echo "Installing pip"
+echo -e $Info"Installing pip"$Off
 print_and_run 'sudo easy_install pip'
 print_and_run 'mkdir -p ~/Library/Python/2.7/lib/python/site-packages'
 print_and_run 'echo "$(brew --prefix)/lib/python2.7/site-packages" >> ~/Library/Python/2.7/lib/python/site-packages/homebrew.pth'
 
 # install XQuartz
-echo "Installing XQuartz"
+echo -e $Info"Installing XQuartz"$Off
 print_and_run 'brew cask install xquartz'
 
 # ssh keys
-echo "Copy your ssh keys in your .ssh directory"
+echo -e $Action"Copy your ssh keys in your .ssh directory"$Off
 read -p "Press [ENTER] once it's done..."
 
 # LaTeX
-echo "Installing mactex"
+echo -e $Info"Installing mactex"$Off
 print_and_run 'brew cask install mactex'
 
 # iPython
-echo "Install iPython"
+echo -e $Info"Installing iPython"$Off
 print_and_run 'pip install ipython'
 
 # install skype
-echo "Installing skype"
+echo -e $Info"Installing skype"$Off
 print_and_run 'brew cask install skype'
 
 # install inkscape
-echo "Installing inkscape"
+echo -e $Info"Installing inkscape"$Off
 print_and_run 'brew cask install inkscape'
 
 # install gimp
-echo "Installing gimp"
+echo -e $Info"Installing gimp"$Off
 print_and_run 'brew cask install gimp'
 
 # install vlc
-echo "Installing vlc"
+echo -e $Info"Installing vlc"$Off
 print_and_run 'brew cask install vlc'
