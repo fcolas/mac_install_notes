7c827e6b9c44b8b7df4e4b6237d445d012d56d16
Francis Colas <francis.colas@inria.fr>
2015-02-20 10:38:05 +0100

    gitlab

---
+++ gitlab_in_vb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function print_and_run {
+	echo "$ "$1
+	$1
+}
+Off="\033[0m"
+Info="\033[1;34m"
+Action="\033[0;31m"
+
+# gitlab
+echo -e $Info"Installing gitlab"$Off
+print_and_run 'sudo apt-get install openssh-server'
+echo -e $Action"Select 'internet site' for postfix setup:"$Off
+read =p "Press [ENTER] to proceed"
+print_and_run 'sudo apt-get install postfix'
+print_and_run 'wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.7.2-omnibus.5.4.2.ci-1_amd64.deb'
+print_and_run 'sudo dpkg -i gitlab_7.7.2-omnibus.5.4.2.ci-1_amd64.deb'
+print_and_run 'sudo gitlab-ctl reconfigure'
+
+# LDAP integration
+echo -e $Action"Activate and setup LDAP by editing /etc/gitlab/gitlab.rb"$Off
+echo "gitlab_rails['ldap_enabled'] = true"
+echo "    host: 'ildap.inria.fr'"
+echo "    uid: 'inriaLogin'"
+echo "    bind_dn: ''"
+echo "    password: ''"
+echo "    base: 'ou=people,dc=inria,dc=fr'"
+read -p "Press [ENTER] when it's done"
+print_and_run 'sudo gitlab-ctl reconfigure'
+
+# redirection
+echo -e $Action"Configure port forwarding on NAT interface of virtual machine:"$Off
+echo "gitlab; TCP; ; 8080; 192.168.56.102; 80"
+echo "ssh; TCP; ; 2222; 192.168.56.102; 22"
+read -p "Press [ENTER] when it's done"
+echo -e $Action"Change url and ssh port by editing /etc/gitlab/gitlab.rb"$Off
+echo "external_url 'http://sancho:8080'"
+echo "..."
+echo "gitlab_rails['gitlab_shell_ssh_port'] = 2222"
+read -p "Press [ENTER] when it's done"
+print_and_run 'sudo gitlab-ctl reconfigure'
--- virtualboxes.sh
+++ virtualboxes.sh
@@ -12,8 +12,16 @@ Action="\033[0;31m"
 echo -e $Info"Installing virtualbox"$Off
 print_and_run 'brew cask install virtualbox'
 echo -e $Action"Launch VirtualBox and create a new network interface (VirtualBox->Preferences->Network->Host-only"$Off
+read -p "Press [ENTER] once it's done..."
 
 # creating virtual machine with ubuntu
 echo -e $Action"Download ubuntu image (14.04.1 server amd64)."$Off
+read -p "Press [ENTER] once it's done..."
 echo -e $Action"Create new virtual machine, add the second network interface as host-only"$Off
+read -p "Press [ENTER] once it's done..."
 echo -e $Action"Run new virtual machine to install ubuntu with OpenSSH."$Off
+read -p "Press [ENTER] once it's done..."
+
+# basic setup
+echo -e $Action"copy .ssh directory and .bash_prompt to new home."$Off
+read -p "Press [ENTER] once it's done..."
